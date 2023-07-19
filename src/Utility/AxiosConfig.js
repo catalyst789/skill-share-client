a08626bf8897d8954ab8ccdf4f23d6c94f12dbc7
Jalaj Kumar Gaur <61074719+catalyst789@users.noreply.github.com>
2023-07-19 12:19:38 +0530

    update server baseUrl in axios configuration

--- src/Utility/AxiosConfig.js
+++ src/Utility/AxiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://skills-share-server-production.up.railway.app/',
+    baseURL: 'https://skills-share-server.onrender.com/',
     headers: {
         "Content-Type": "application/json"
     }
